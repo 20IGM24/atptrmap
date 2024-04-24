1d1bbaa9f864a04a52aeac5d9a8597b7d49b659e
atptrmap <atptrmap@gmail.com>
2024-04-24 18:18:07 -0500

    redo

--- azjavascript.js
+++ azjavascript.js
@@ -1,15 +1,14 @@
-$(document).ready(function(){
-  $(".hide").hide();
-  
- 
-  $(".icave").click(function(){
-    $(".hide").hide();
-    $(".cave-txt").fadeIn(600);
-  });
-  
-  $(".ihorseman").click(function(){
-    $(".hide").hide();
-    $(".horseman-txt").fadeIn(600);
-  });
-  
+// Add click event listener to the extract-cave image
+document.getElementById('extract-cave').addEventListener('click', function() {
+  // Show the cave-txt popup window by removing the hide class
+  document.getElementById('cave-txt').classList.remove('hide');
+  // Update the content of the cave-txt popup window
+  document.getElementById('cave-txt').innerHTML = `
+    <h>Cave extraction</h>
+    <p>Timer:60 seconds</p>
+    <p>Cost:Free</p>
+    <p>Enemy:None</p>
+    <!-- Reference to the image of the location -->
+    <img class="extraction" src="data/locations/cave.png" alt="cave extraction">
+  `;
 });
