65b346809fd7089d2ff969a3dad6c9d7da962a6c
atptrmap <atptrmap@gmail.com>
2024-04-24 15:42:35 -0500

    updated website 4/24

---
+++ azjavascript.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $(".hide").hide();
+  
+ 
+  $(".icave").click(function(){
+    $(".hide").hide();
+    $(".cave-txt").toggle();
+  });
+  
+  $(".ihorseman").click(function(){
+    $(".hide").hide();
+    $(".horseman-txt").toggle();
+  });
+  
+});
