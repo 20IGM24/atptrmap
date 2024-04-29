43adbd1117f73eb6cc26a641870e8006906cca6c
atptrmap <atptrmap@gmail.com>
2024-04-29 10:34:59 -0500

    v1 -upload
    Gitas86

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
+
+export default defineConfig({
+  base: "/atptrmap/",
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+        attributions_en: resolve(__dirname, "attributions/en.html"),
+        attributions_es: resolve(__dirname, "attributions/es.html"),
+        attributions_uk: resolve(__dirname, "attributions/uk.html"),
+        arizona_forest: resolve(__dirname, "maps/arizona-forest.html"),
+      },
+    },
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
+  plugins: [vitePluginFaviconsInject("./favicon.ico")],
+});
