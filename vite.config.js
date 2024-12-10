657996efa136599e21b9804ca602b6cf237e99e9
atptrmap <167710288+atptrmap@users.noreply.github.com>
2024-12-10 15:43:23 -0600

    Test 6
    Commented out row 3 to see output error
    
    Signed-off-by: atptrmap <167710288+atptrmap@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
+//import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
 
 export default defineConfig({
   base: "/atptrmap/",
