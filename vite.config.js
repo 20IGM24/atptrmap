8469861485bf8fef2420b7d4c37a97ecd9949b4f
atptrmap <167710288+atptrmap@users.noreply.github.com>
2024-12-10 15:26:45 -0600

    Test 2
    removed ...arizona-forest
    
    Signed-off-by: atptrmap <167710288+atptrmap@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ export default defineConfig({
         attributions_en: resolve(__dirname, "attributions/en.html"),
         attributions_es: resolve(__dirname, "attributions/es.html"),
         attributions_ua: resolve(__dirname, "attributions/ua.html"),
-        arizona_forest: resolve(__dirname, "maps/arizona-forest.html"),
         arizona_forest: resolve(__dirname, "maps/tennessee-hills.html"),
         arizona_forest: resolve(__dirname, "maps/mountain-riffs.html"),
       },
