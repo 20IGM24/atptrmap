8a76e8dc5d941a70ab6a318109bb2e7c26a31f3e
atptrmap <167710288+atptrmap@users.noreply.github.com>
2024-12-10 15:50:55 -0600

    Test 7
    removed comment on row 3
    
    Signed-off-by: atptrmap <167710288+atptrmap@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
-//import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
+import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
 
 export default defineConfig({
   base: "/atptrmap/",
@@ -13,7 +13,7 @@ export default defineConfig({
         attributions_es: resolve(__dirname, "attributions/es.html"),
         attributions_ua: resolve(__dirname, "attributions/ua.html"),
         tennessee_hills: resolve(__dirname, "maps/tennessee-hills.html"),
-         arizona_forest: resolve(__dirname, "maps/arizona-forest.html")
+         arizona_forest: resolve(__dirname, "maps/arizona-forest.html"),
       },
     },
   },
